514ab4ba485b77fda52d03440e0e2fa907272caa
attila <attilagardo@gmail.com>
2021-01-14 13:19:53 +0100

    user working 100% except security

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UsersEditRequest;
 use App\Http\Requests\UsersRequest;
 use App\Photo;
 use App\Role;
@@ -50,16 +51,18 @@ class AdminUsersController extends Controller
     {
         //
         //User::create($request->all());
-
-        $input = $request->all();
+        if(trim($request->password == '')){
+            $input = $request->except('password');
+        } else {
+            $input = $request->all();
+            $input['password'] = bcrypt($request->password);
+        }
         if($file = $request->file('photo_id')){
             $name = time().$file->getClientOriginalName();
             $file->move('images',$name);
             $photo = Photo::create(['file'=>$name]);
             $input['photo_id'] = $photo->id;
         }
-
-        $input['password'] = bcrypt($request->password);
 
         User::create($input);
 
@@ -91,8 +94,12 @@ class AdminUsersController extends Controller
     public function edit($id)
     {
         //
+        $user = User::findOrFail($id);
 
-        return view('admin.users.edit');
+        $roles = Role::lists('name','id')->all();
+
+
+        return view('admin.users.edit', compact('user','roles'));
     }
 
     /**
@@ -102,9 +109,29 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UsersEditRequest $request, $id)
     {
         //
+        $user = User::findOrFail($id);
+
+        if(trim($request->password == '')){
+            $input = $request->except('password');
+        } else {
+            $input = $request->all();
+            $input['password'] = bcrypt($request->password);
+        }
+
+        if($file = $request->file('photo_id')){
+            $name = time().$file->getClientOriginalName();
+            $file->move('images',$name);
+            $photo = Photo::create(['file'=>$name]);
+            $input['photo_id'] = $photo->id;
+        }
+
+        $input['password'] = bcrypt($request->password);
+        $user->update($input);
+
+        return redirect('/admin/users');
     }
 
     /**
--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -4,48 +4,52 @@
 
 @section('content')
 
-    <h1>Creating Users</h1>
+    <div class="row">
 
-    {!! Form::open(['method'=>'POST','action'=>'AdminUsersController@store','files'=>true]) !!}
+        <h1>Creating Users</h1>
 
-    {{csrf_field()}}
+        {!! Form::open(['method'=>'POST','action'=>'AdminUsersController@store','files'=>true]) !!}
 
-    <div class="form-group">
-        {!! Form::label('name','Name') !!}
-        {!! Form::text('name',null,['class'=>'form-control']) !!}
+        {{csrf_field()}}
+
+        <div class="form-group">
+            {!! Form::label('name','Name') !!}
+            {!! Form::text('name',null,['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('email','E-mail') !!}
+            {!! Form::email('email',null,['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('role_id','Role') !!}
+            {!! Form::select('role_id',[''=>'Choose Options']+ $roles,null,['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('is_active','Status') !!}
+            {!! Form::select('is_active',array(1=>'Active',0=>'Not Active'),0,['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('photo_id','Photo') !!}
+            {!! Form::file('photo_id', ['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('password','Password') !!}
+            {!! Form::password('password', ['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::submit('Create User',['class'=>'btn btn-primary']) !!}
+        </div>
+
+        {!! Form::close() !!}
+
+    <div class="row">
+        @include('includes.form_error')
     </div>
-
-    <div class="form-group">
-        {!! Form::label('email','E-mail') !!}
-        {!! Form::email('email',null,['class'=>'form-control']) !!}
-    </div>
-
-    <div class="form-group">
-        {!! Form::label('role_id','Role') !!}
-        {!! Form::select('role_id',[''=>'Choose Options']+ $roles,null,['class'=>'form-control']) !!}
-    </div>
-
-    <div class="form-group">
-        {!! Form::label('is_active','Status') !!}
-        {!! Form::select('is_active',array(1=>'Active',0=>'Not Active'),0,['class'=>'form-control']) !!}
-    </div>
-
-    <div class="form-group">
-        {!! Form::label('photo_id','Photo') !!}
-        {!! Form::file('photo_id', ['class'=>'form-control']) !!}
-    </div>
-
-    <div class="form-group">
-        {!! Form::label('password','Password') !!}
-        {!! Form::password('password', ['class'=>'form-control']) !!}
-    </div>
-
-    <div class="form-group">
-        {!! Form::submit('Create User',['class'=>'btn btn-primary']) !!}
-    </div>
-
-    {!! Form::close() !!}
-
-    @include('includes.form_error')
 
 @endsection
